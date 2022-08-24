35c720221be137df14d0881878ab592a20d7e8ee
Nicola Field <nicola.field2@gmail.com>
2022-08-24 14:04:17 +0100

    Setting up restaurant routes, controller and views for index, show, new, create

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+bistrot = {name: "Bistrot Pierre", address: "Oyster Wharf, Mumbles", category: "french"}
+black_boy =  {name: "Black Boy", address: "Killay Precinct, Swansea", category: "italian"}
+castellamare = {name: "Castellamare", address: "Limeslade Bay, Mumbles", category: "italian"}
+thai_bach = {name: "Thai Bach", address: "Brynymor Road, Uplands", category: "chinese"}
+nishimura = {name: "Nishimura", address: "Brynymor Road, Uplands", category: "japanese"}
+
+[bistrot, black_boy, castellamare, thai_bach, nishimura].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts "Finished!"
